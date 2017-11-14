a52ec0632964e1ae76f18ad11d32a6cce7fc65ea
Dimonclassic05 <32394657+Dimonclassic05@users.noreply.github.com>
2017-11-14 16:42:49 +0300

    новый

--- Homeworks/Homework_5/5-1complex/main.cpp
+++ Homeworks/Homework_5/5-1complex/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 
 using namespace std;
 
@@ -9,53 +10,72 @@ struct Complex
     int y;
 };
 
-void sum(const Complex& number1, const Complex& number2);
-void sub(const Complex& number1, const Complex& number2);
-void mul(const Complex& number1, const Complex& number2);
-void div(const Complex& number1, const Complex& number2);
+Complex sum(const Complex& number1, const Complex& number2);
+Complex sub(const Complex& number1, const Complex& number2);
+Complex mul(const Complex& number1, const Complex& number2);
+Complex div(const Complex& number1, const Complex& number2);
+void output(const Complex& number);
 
 
 int main()
 {
-    Complex number1 {2,2};
-    Complex number2 {1,1};
-    sum(number1,number2);
-    sub(number1,number2);
-    mul(number1,number2);
-    div(number1,number2);
+    Complex number1 {1,1};
+    Complex number2 {2,2};
+    Complex sum = sum(number1,number2);
+    Complex sub = sub(number1,number2);
+    Complex mul = mul(number1,number2);
+    Complex div = div(number1,number2);
+    output(sum);
+    output(sub);
+    output(mul);
+    output(div);
+
+    system("pause");
     return 0;
 }
 
 
 
-void sum(const Complex& number1, const Complex& number2)
+Complex sum(const Complex& number1, Complex& number2)
 {
     Complex sum;
     sum.x = number1.x + number2.x;
     sum.y = number1.y + number2.y;
-    cout << sum.x << "+i" << sum.y << std::endl;
+    return sum;
 }
 
-void sub(const Complex& number1, const Complex& number2)
+Complex sub(const Complex& number1, const Complex& number2)
 {
     Complex sub;
     sub.x = number1.x - number2.x;
     sub.y = number1.y - number2.y;
-    cout << sub.x << "+i" << sub.y << std::endl;
+    return sub;
 }
 
-void mul(const Complex& number1, const Complex& number2)
+Complex mul(const Complex& number1,const Complex& number2)
 {
     Complex mul;
     mul.x = (number1.x*number2.x) - (number1.y*number2.y);
     mul.y = (number1.y*number2.x) + (number1.x*number2.y);
-    cout << mul.x << "+i" << mul.y << std::endl;
+    return mul;
 }
 
-void div(const Complex& number1, const Complex& number2)
+Complex div(const Complex& number1,const Complex& number2)
 {
     Complex div;
     div.x = ((number1.x*number2.x) + (number1.y*number2.y))/((number2.x*number2.x)+ (number2.y*number2.y));
-    div.y = ((number1.y*number2.x)-(number1.x*number2.y))/((number2.x*number2.x)+ (number2.y*number2.y));
-    cout << div.x << "+i" << div.y << std::endl;
+    div.y = ((number1.y*number2.x) - (number1.x*number2.y))/((number2.x*number2.x)+ (number2.y*number2.y));
+    return div;
+}
+
+void output(const Complex& number)
+{
+    if(number.y >= 0)
+    {
+    cout << number.x << "+i" << number.y << std::endl;
+    }
+    else
+    {
+    cout << number.x << "-i" << abs(number.y) << std::endl;
+    }
 }
