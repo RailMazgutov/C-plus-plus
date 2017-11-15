cb9ee5a53bd8fce1f791e468ef7822cd3b8926c0
Dimonclassic05 <32394657+Dimonclassic05@users.noreply.github.com>
2017-11-15 17:27:56 +0300

    5-2

---
+++ Homeworks/Homework_5/5-2sortstruct/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstdlib>
+
+void sort(int* array, int size);
+
+struct Massiv
+{
+  int* array;
+  int size;
+};
+Massiv input(int* array);
+
+int main()
+{
+    Massiv param = input(param.array);
+}
+
+
+    Massiv input(int* array)
+    {
+        int size = 0;
+        std::cout << "enter the number " ;
+        int num;
+        std::cin >> num;
+        while(num > 0)
+        {
+             size += 1;
+             int* arraypom = new int[size];
+             for (int i = 0; i < (size-1); ++i)
+             {
+                arraypom[i] = array[i];
+             }
+             arraypom[size-1] = num;
+             delete[] array;
+             array = arraypom;
+
+             std::cout << "enter the number " ;
+             std::cin >> num;
+        }
+
+
+        Massiv ret;
+        ret.array = array;
+        ret.size = size;
+        return ret;
+    }
+
+
+
+void sort (int* array, int size)
+{
+    {
+        if (size == 1)
+        {
+            return;
+        }
+        int sizea = size/2;
+        int sizeb = size - sizea;
+        int* arraya = new int[sizea];
+        int* arrayb = new int[sizeb];
+
+        for (int i = 0; i < sizea; ++i)
+        {
+            arraya[i] = array[i];
+        }
+
+        for (int i = sizea; i < (size); ++i)
+        {
+            arrayb[i-sizea] = array[i];
+        }
+
+            sort(arraya, sizea);
+            sort(arrayb, sizeb);
+         int c = 0;
+         int d = 0;
+            for (int i = 0; i < size; ++i)
+            {
+
+                if ((c == sizea) || (d == sizeb))
+               {
+                  if (c == sizea)
+                  {
+                       array[i] = arrayb[d];
+                      d += 1;
+                  }
+                  else
+                    {
+                       array[i] = arraya[c];
+                       c += 1;
+                    }
+                }
+                else
+                {
+
+                 if (arraya[c] > arrayb[d])
+                    {
+                       array[i] = arrayb[d];
+                        d += 1;
+                    }
+                 else
+                    {
+                      array[i] = arraya[c];
+                      c += 1;
+                    }
+                }
+            }
+        delete[] arraya;
+        delete[] arrayb;
+    }
+}
